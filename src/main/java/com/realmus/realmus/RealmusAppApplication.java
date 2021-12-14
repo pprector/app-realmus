ed8058bdb2fcc93168d8fbfbd63fbad3af58ecd9
smallant <15617889809@163.com>
2021-12-14 21:06:02 +0800

    feat

---
+++ src/main/java/com/realmus/realmus/RealmusAppApplication.java
@@ -0,0 +1,13 @@
+package com.realmus.realmus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealmusAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealmusAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/realmus/realmus/RealmusAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realmus.realmus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealmusAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
