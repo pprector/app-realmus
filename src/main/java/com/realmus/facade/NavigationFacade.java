5f7c1ff65342d42fb8773058ee9b4b75a33c9737
smallant <15617889809@163.com>
2021-12-20 22:24:29 +0800

    feat：添加接口

---
+++ src/main/java/com/realmus/facade/NavigationFacade.java
@@ -0,0 +1,12 @@
+package com.realmus.facade;
+
+/**
+ * realmus.com
+ *
+ * @Author hkpeng
+ * @Description 网站标题栏请求接口 
+ * @Date 2021/12/20 22:23
+ * @Version 1.0
+ */
+public interface NavigationFacade {
+}
